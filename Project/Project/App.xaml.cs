8b8f6fc0afaaafda343eb0c726f31baf759fc0b4
Akemax <silver.maks10@gmail.com>
2021-06-04 02:18:52 +0300

    Добавьте файлы проекта.

---
+++ Project/Project/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //MainPage = new MainPage();
+            //MainPage = new Project.FirstPage();
+            //MainPage = new FirstPage();
+           MainPage = new NavigationPage(new Project.FirstPage());
+        }
+
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnSleep()
+        {
+
+        }
+
+        protected override void OnResume()
+        {
+
+        }
+    }
+}
+
---
+++ Project/Project/FirstPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Label1.Text = Entry1.Text;
+        }
+
+        private async void Settings_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Setting());
+        }
+
+        private void Exit_Clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private async void Button_Level_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Level());
+        }
+    }
+}
